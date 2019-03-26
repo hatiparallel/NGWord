ad65628c3154dc97b37e37ce41fdd116b1e44fdb
unknown <pneumoulramicro3.7@gmail.com>
2019-03-26 16:08:57 +0900

    made WordsDecider

---
+++ app/src/main/java/com/parallel/hati/ngword/WordsDecider.kt
@@ -0,0 +1,26 @@
+package com.parallel.hati.ngword
+
+import java.util.*
+
+class WordsDecider {
+    companion object {
+        val defaultWords: Array<String> = arrayOf( "男", "女", "目", "机", "椅子", "トイレ" )
+    }
+
+    fun shuffleNumber(resultSize: Int, arrSize: Int): Array<Int> {
+        val arr = Array<Int>(arrSize, { it }).toMutableList()
+        val result: MutableList<Int> = mutableListOf()
+        val rand = Random()
+
+        for (i in 0..resultSize-1) {
+            result.add(arr.removeAt(rand.nextInt(arrSize - 1 - i)))
+        }
+
+        return result.toTypedArray()
+    }
+
+    fun chooseDefaultWords(count: Int): Array<String> {
+        val shuffled = shuffleNumber(count, defaultWords.size)
+        return Array<String>(count, { defaultWords[shuffled[it]] })
+    }
+}
