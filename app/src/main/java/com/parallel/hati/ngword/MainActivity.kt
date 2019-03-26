bb7f6b23968181c1cfff8414302b32fe33ae6061
unknown <pneumoulramicro3.7@gmail.com>
2019-03-26 13:45:40 +0900

    make count changable

--- app/src/main/java/com/parallel/hati/ngword/MainActivity.kt
+++ app/src/main/java/com/parallel/hati/ngword/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
@@ -12,14 +14,34 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var count = 3
+        val count_view = findViewById< TextView>(R.id.count_view)
+
+        findViewById<ImageButton>(R.id.decrease_button).setOnClickListener {
+            if (count > 2) {
+                count -= 1
+                count_view.setText("${count}人")
+            } else {
+                Toast.makeText(applicationContext, "人数は最小で2人までです", Toast.LENGTH_LONG).show()
+            }
+        }
+
+        findViewById<ImageButton>(R.id.increase_button).setOnClickListener {
+            if (count < 16) {
+                count += 1
+                count_view.setText("${count}人")
+            } else {
+                Toast.makeText(applicationContext, "人数は最大で16人までです", Toast.LENGTH_LONG).show()
+            }
+        }
+
         findViewById<Button>(R.id.howto).setOnClickListener {
             Toast.makeText(applicationContext, "hello", Toast.LENGTH_SHORT).show()
         }
 
         findViewById<Button>(R.id.start).setOnClickListener {
-            Toast.makeText(applicationContext, "start", Toast.LENGTH_LONG).show()
             intent = Intent(this, RegisterActivity::class.java)
-            intent.putExtra("count", 3)
+            intent.putExtra("count", count)
             startActivity(intent)
         }
     }
